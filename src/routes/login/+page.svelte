5c847d3636f43b691ea510894a83de665a957cd0
Jensen Lai <ninnu.0123p@gmail.com>
2023-12-05 14:11:47 +1000

    change code for picoCSS v2

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,8 +1,10 @@
 <main class="container">
 	<h1>Login</h1>
 	<form method="POST">
-		<input type="text" name="username" placeholder="Username">
-		<input type="password" name="password" placeholder="Password">
-		<button type="submit">Submit</button>
+		<div role="group">
+			<input type="text" name="username" placeholder="Username">
+			<input type="password" name="password" placeholder="Password">
+			<button type="submit">Submit</button>
+		</div>
 	</form>
 </main>
--- src/routes/logout/+page.svelte
+++ src/routes/logout/+page.svelte
@@ -1,6 +1,5 @@
 <main class="container">
 	<h1>Are you sure you want to log out?</h1>
-	<a href="/" role="button" class="secondary full">No</a>
 	<form method="POST">
 		<button type="submit">Yes</button>
 	</form>
